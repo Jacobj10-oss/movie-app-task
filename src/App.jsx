0a2eae2ce79da897854a35f6e40ad19e85e3910e
Jacobj10-oss <jsaves030@gmail.com>
2026-02-07 23:03:31 +0530

    presentation component

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,36 @@ function App() {
 
   return (
     <>
-    <nav>
+    <Movies/>
+    
+    {/* <nav>
         <Link to="/about">About</Link>
-    </nav>
-      <Routes>
+    </nav> */}
+      {/* <Routes>
       <Route path="/" element={<App />} />
-    </Routes>
+    </Routes> */}
     </>
+  )
+}
+
+function Movies(){
+  return(
+    <div className="movies-container">
+      <div className="movie-poster">
+        <img src="https://assets-in.bmscdn.com/discovery-catalog/events/et00401449-yvzgkbpdca-portrait.jpg" alt="" />
+      </div>
+      <div className="movie-name-rating">
+        <div className="movie-name">
+          <h3>Border 2</h3>
+          <h3>⭐7.5</h3>
+        </div>
+      </div>
+      <div className="movie-summary">
+        <p>A massive war drama sequel set during the 1971 Indo-Pak war. Sunny Deol returns as Major Kuldip Singh Chandpuri to lead a new battalion in a high-stakes battle for sovereignty, featuring intense tank warfare and patriotic sacrifice.</p>
+
+      </div>
+
+    </div>
   )
 }
 
